45ea3dbd3bcb977869450b33f2478b912c8cc299
David Teller <david.teller@pasqal.com>
2025-05-09 16:33:42 +0200

    Towards a postprocessor

---
+++ mis/pipeline/postprocessor.py
@@ -0,0 +1,19 @@
+import abc
+
+from mis.shared.types import MISSolution
+
+
+class Postprocessor(abc.ABC):
+    @abc.abstractmethod
+    def postprocess(self, solution: MISSolution) -> MISSolution | None:
+        """
+        Post-process a solution, typically to improve its quality.
+
+        May return `None` if the solution is deemed unacceptable.
+        """
+        ...
+
+
+class EmptyPostProcessor(Postprocessor):
+    def postprocess(self, solution: MISSolution) -> MISSolution | None:
+        return solution
