04f8126a34248753c22193ff93d0e7975493078f
Richie Yeung <richie.yeung1@gmail.com>
2025-05-28 13:58:06 +0100

    Add tests

---
+++ tests/test_dimacs.py
@@ -0,0 +1,37 @@
+from mis.data.graphs import load_dimacs
+
+
+def test_hexagon():
+    """
+    Test loading the C6 from a DIMACS file.
+    """
+    dataset = load_dimacs("mis/data/dimacs/hexagon.txt")
+    assert dataset.instance.graph.number_of_nodes() == 6
+    assert dataset.instance.graph.number_of_edges() == 6
+    assert len(dataset.solutions) > 0
+    assert all(isinstance(solution, list) for solution in dataset.solutions)
+    assert all(len(solution) <= 3 for solution in dataset.solutions)
+
+
+def slowtest_petersen():
+    """
+    Test loading the Petersen graph from a DIMACS file.
+    """
+    dataset = load_dimacs("mis/data/dimacs/petersen.txt")
+    assert dataset.instance.graph.number_of_nodes() == 10
+    assert dataset.instance.graph.number_of_edges() == 15
+    assert len(dataset.solutions) > 0
+    assert all(isinstance(solution, list) for solution in dataset.solutions)
+    assert all(len(solution) <= 5 for solution in dataset.solutions)  # Petersen graph has a maximum independent set of size 5
+
+
+def slowtest_a265032():
+    """
+    Test loading the Petersen graph from a DIMACS file.
+    """
+    dataset = load_dimacs("mis/data/dimacs/a265032_1dc.64.txt")
+    assert dataset.instance.graph.number_of_nodes() == 64
+    assert dataset.instance.graph.number_of_edges() == 543
+    assert len(dataset.solutions) > 0
+    assert all(isinstance(solution, list) for solution in dataset.solutions)
+    assert all(len(solution) <= 10 for solution in dataset.solutions)  # graph has a maximum independent set of size 10
