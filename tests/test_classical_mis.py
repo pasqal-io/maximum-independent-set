4e8b777cb9b7bc4bae7d7ef1ed124366a14f2f03
RolandMacDoland <9250798+RolandMacDoland@users.noreply.github.com>
2025-06-03 09:22:44 +0100

    [Docs] Tutorial and tests for base dataset (#28)
    * Add DIMACS test file and converter fixture.
    
    * Add test to check on base DIMACS MIS benchmark data.
    
    * Formatting and type annotation.
    
    * Add DIMACS datasets for tutorial.
    
    * Adding basic examples for DIMACS 32-nodes dataset on various backends.
    
    * Lint.
    
    * Add a test for a 32-node DIMACS dataset.
    
    * Update notebook.
    
    * Lint.
    
    * Apply feedback.
    
    * Update examples/tutorial 1a - Using a Quantum Device to solve MIS.ipynb
    
    Co-authored-by: David Teller <D.O.Teller@gmail.com>
    
    * Update examples/tutorial 1a - Using a Quantum Device to solve MIS.ipynb
    
    Co-authored-by: David Teller <D.O.Teller@gmail.com>
    
    * Move files to better named directory.
    
    * Moved files to better named directory.
    
    * Fix test files path.
    
    * Add comment and compare set of nodes.
    
    * Lint.
    
    * Reactivate testing notebooks.
    
    * Enable tutorial in docs.
    
    * Fix formatting error and add more natative to the use of the remote backend.
    
    * Lint.
    
    * Comment on generated results.
    
    * Lint.
    
    * Remove copying obsolete files.
    
    * Reformat introductory text.
    
    * Run test for single notebook and reduce python versions for fastest execution.
    
    ---------
    
    Co-authored-by: David Teller <D.O.Teller@gmail.com>

--- tests/test_classical_mis.py
+++ tests/test_classical_mis.py
@@ -1,6 +1,7 @@
 from typing import Callable
 import networkx as nx
 import pytest
+from pathlib import Path
 
 # Define classical solver configuration
 from mis.solver.solver import MISInstance, MISSolver
@@ -8,6 +9,79 @@ from mis.pipeline.config import SolverConfig
 from mis.shared.types import MethodType
 from mis.pipeline.kernelization import Kernelization
 from mis.pipeline.maximization import Maximization
+
+TEST_DIMACS_FILES_DIR = Path.cwd() / "tests/test_files/dimacs"
+
+
+@pytest.mark.flaky(max_runs=5)
+@pytest.mark.parametrize(
+    "dimacs_to_nx", [TEST_DIMACS_FILES_DIR / "a265032_1tc.32.txt"], indirect=True
+)
+@pytest.mark.parametrize("preprocessor", [None, lambda graph: Kernelization(graph)])
+def test_for_dimacs_32_node_graph(
+    dimacs_to_nx: nx.Graph, preprocessor: None | Callable[[nx.Graph], Kernelization]
+) -> None:
+    """
+    Classical MIS solver for a standard graph benchmark dataset in DIMACS format.
+
+    Can be found here: https://oeis.org/A265032/a265032.html
+    """
+    graph = dimacs_to_nx
+    assert graph.number_of_nodes() == 32
+    assert graph.number_of_edges() == 68
+
+    config = SolverConfig(method=MethodType.EAGER, max_iterations=1, preprocessor=None)
+
+    # Create the MIS instance
+    instance = MISInstance(graph)
+
+    # Run the solver
+    solver = MISSolver(instance, config)
+    solutions = solver.solve().result()
+
+    assert len(solutions[0].nodes) == 12
+
+    # Check the solution is genuinely an independent set.
+    kernel = Kernelization(graph=graph)
+    assert kernel.is_independent(solutions[0].nodes)
+
+
+@pytest.mark.flaky(max_runs=5)
+@pytest.mark.parametrize(
+    "dimacs_to_nx", [TEST_DIMACS_FILES_DIR / "a265032_1dc.64.txt"], indirect=True
+)
+@pytest.mark.parametrize("preprocessor", [None, lambda graph: Kernelization(graph)])
+def test_for_dimacs_64_node_graph(
+    dimacs_to_nx: nx.Graph, preprocessor: None | Callable[[nx.Graph], Kernelization]
+) -> None:
+    """
+    Classical MIS solver for a standard graph benchmark dataset in DIMACS format.
+
+    Can be found here: https://oeis.org/A265032/a265032.html
+    """
+    graph = dimacs_to_nx
+    assert graph.number_of_nodes() == 64
+    assert graph.number_of_edges() == 543
+
+    config = SolverConfig(method=MethodType.EAGER, max_iterations=1, preprocessor=preprocessor)
+
+    # Create the MIS instance
+    instance = MISInstance(graph)
+
+    # Run the solver
+    solver = MISSolver(instance, config)
+    solutions = solver.solve().result()
+
+    # Check the solution is genuinely an independent set.
+    kernel = Kernelization(graph=graph)
+
+    # In this case, pre-processing the data reveals a better solution.
+    if preprocessor is None:
+        assert set(solutions[0].nodes) == {1, 4, 42, 11, 16, 56, 25, 61}
+        assert kernel.is_independent(solutions[0].nodes)
+    else:
+        assert set(solutions[0].nodes) == {64, 1, 4, 13, 16, 22, 47, 49, 52}
+        assert kernel.is_independent(solutions[0].nodes)
 
 
 @pytest.mark.parametrize("preprocessor", [None, lambda graph: Kernelization(graph)])
