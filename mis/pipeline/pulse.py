1ee3d2d94cfb20f7dc6d79225fee67d0311da6eb
David Teller <david.teller@pasqal.com>
2025-06-13 13:02:00 +0200

    [Cleanup] Cleaning up pulse shaper - resolves #20
    This should future evolutions a bit simpler

--- mis/pipeline/pulse.py
+++ mis/pipeline/pulse.py
@@ -2,11 +2,9 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 from abc import ABC, abstractmethod
-from typing import Any
 
 from networkx.classes.reportviews import DegreeView
 from pulser import InterpolatedWaveform, Pulse as PulserPulse
-from pulser.devices import Device
 
 from mis.pipeline.config import SolverConfig
 
@@ -58,104 +56,72 @@ class DefaultPulseShaper(BasePulseShaper):
     A simple pulse shaper.
     """
 
-    def _get_interactions(
-        self, pos: np.ndarray, graph: nx.Graph, device: Device, index_by_node: dict[Any, int]
-    ) -> tuple[list[float], list[float]]:
-        """Calculate the interaction strengths for connected and disconnected
-            nodes.
-
-        Args:
-            pos (np.ndarray): The position of the nodes.
-            graph (nx.Graph): The associated graph.
-            device (BaseDevice): Device used to calculate interaction coeff.
-            index_by_node: A map from node to its index in the list of nodes.
-
-        Returns:
-            tuple[list[float], list[float]]: Connected interactions,
-                Disconnected interactions
+    def generate(self, config: SolverConfig, register: Register) -> Pulse:
         """
+        Return a simple constant waveform pulse
+        """
+
+        device = config.device
+        assert device is not None
+
+        # Cache mapping node value -> node index.
+        index_by_node = {node: i for (i, node) in enumerate(register.graph.nodes)}
+
+        graph = register.graph
+        pos = register.register.sorted_coords
 
         def calculate_edge_interaction(edge: tuple[int, int]) -> float:
             pos_a, pos_b = pos[index_by_node[edge[0]]], index_by_node[pos[edge[1]]]
             return float(device.interaction_coeff / (euclidean(pos_a, pos_b) ** 6))
 
+        # Interaction strength for connected nodes.
         connected = [calculate_edge_interaction(edge) for edge in graph.edges()]
+
+        # Interaction strength for disconnected nodes.
         disconnected = [calculate_edge_interaction(edge) for edge in nx.complement(graph).edges()]
 
-        return connected, disconnected
-
-    def _calc_bounds(self, reg: Register, device: Device) -> _Bounds:
-        index_by_node = {node: i for (i, node) in enumerate(reg.graph.nodes)}
-        _, disconnected = self._get_interactions(
-            pos=reg.register.sorted_coords,
-            graph=reg.graph,
-            device=device,
-            index_by_node=index_by_node,
-        )
-        u_min, u_max = self._interaction_bounds(
-            pos=reg.register.sorted_coords,
-            graph=reg.graph,
-            device=device,
-            index_by_node=index_by_node,
-        )
-        max_amp_device = device.channels["rydberg_global"].max_amp or np.inf
-        maximum_amplitude = min(max_amp_device, u_max + 0.8 * (u_min - u_max))
-
-        degree = reg.graph.degree
-        assert isinstance(degree, DegreeView)
-        d_min = min(dict(degree).values())
-        d_max = max(dict(degree).values())
-        det_max_theory = (d_min / (d_min + 1)) * u_min
-        det_min_theory = sum(sorted(disconnected)[-d_max:])
-        det_final_theory = max([det_max_theory, det_min_theory])
-        det_max_device = device.channels["rydberg_global"].max_abs_detuning or np.inf
-        final_detuning = min(det_final_theory, det_max_device)
-
-        return _Bounds(maximum_amplitude=maximum_amplitude, final_detuning=final_detuning)
-
-    def _interaction_bounds(
-        self,
-        pos: np.ndarray,
-        graph: nx.Graph,
-        device: Device,
-        index_by_node: dict[Any, int],
-    ) -> tuple[float, float]:
-        """Calculates U_min and U_max given the positions. It uses the edges
-        of the graph. U_min corresponds to minimal energy of two nodes
-        connected in the graph. U_max corresponds to maximal energy of two
-        nodes NOT connected in the graph."""
-        connected, disconnected = self._get_interactions(
-            pos, graph, device, index_by_node=index_by_node
-        )
+        # Determine the minimal energy between two connected nodes.
         if len(connected) == 0:
             u_min = 0
         else:
             u_min = np.min(connected)
+
+        # Determine the maximal energy between two disconnected nodes.
         if len(disconnected) == 0:
             u_max = np.inf
         else:
             u_max = np.max(disconnected)
-        return u_min, u_max
 
-    def generate(self, config: SolverConfig, register: Register) -> Pulse:
-        """
-        Method to return a simple constant waveform pulse
-        """
-        device = config.device
-        assert device is not None
+        max_amp_device = device.channels["rydberg_global"].max_amp or np.inf
+        maximum_amplitude = min(max_amp_device, u_max + 0.8 * (u_min - u_max))
+        # FIXME: Why 0.8?
+
+        # Compute min/max degrees
+        degree = register.graph.degree
+        assert isinstance(degree, DegreeView)
+        d_min = None
+        d_max = None
+        for deg in degree:
+            if d_min is None or deg < d_min:
+                d_min = deg
+            if d_max is None or deg > d_max:
+                d_max = deg
+        assert d_min is not None
+        assert d_max is not None
+        det_max_theory = (d_min / (d_min + 1)) * u_min
+        det_min_theory = sum(sorted(disconnected)[-d_max:])
+        det_final_theory = max(det_max_theory, det_min_theory)
+        det_max_device = device.channels["rydberg_global"].max_abs_detuning or np.inf
+        final_detuning = min(det_final_theory, det_max_device)
 
         duration_us = self.duration_us
         if duration_us is None:
             duration_us = device.max_sequence_duration
 
-        bounds = self._calc_bounds(reg=register, device=device)
-
         amplitude = InterpolatedWaveform(
-            duration_us, [1e-9, bounds.maximum_amplitude, 1e-9]
+            duration_us, [1e-9, maximum_amplitude, 1e-9]
         )  # FIXME: This should be 0, investigate why it's 1e-9
-        detuning = InterpolatedWaveform(
-            duration_us, [-bounds.final_detuning, 0, bounds.final_detuning]
-        )
+        detuning = InterpolatedWaveform(duration_us, [-final_detuning, 0, final_detuning])
         rydberg_pulse = PulserPulse(amplitude, detuning, 0)
         # Pulser overrides PulserPulse.__new__ with an exotic type, so we need
         # to help mypy.
