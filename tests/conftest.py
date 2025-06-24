236d7d54799819c5c301faf5ed2b6e977899d315
David Teller <D.O.Teller@gmail.com>
2025-06-24 16:42:31 +0200

    [Feature] Support arbitrary labels for graphs - resolves #75, #74 (#87)

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,6 @@
 import pytest
 
 import networkx as nx
-from pathlib import Path
-from mis.data.graphs import load_dimacs
-
-
-@pytest.fixture
-def dimacs_to_nx(request: pytest.FixtureRequest) -> tuple[nx.Graph, int, int, int]:
-    file_path, num_nodes, num_edges, max_independent_set_size = request.param
-    dataset = load_dimacs(Path(file_path))
-    graph = dataset.instance.graph
-    return graph, num_nodes, num_edges, max_independent_set_size
 
 
 @pytest.fixture
