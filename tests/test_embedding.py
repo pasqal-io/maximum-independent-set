0ce0895ac47e0aaa158c07dc04c3573abde545fc
Charles Moussa <c.moussa@pasqal.com>
2025-10-20 10:19:24 +0200

    add test embedding

---
+++ tests/test_embedding.py
@@ -0,0 +1,32 @@
+import pytest
+import networkx as nx
+from mis import BackendConfig
+from mis.pipeline.config import SolverConfig
+from mis.pipeline.embedder import DefaultEmbedder, OptimizedEmbedder
+from mis.solver.solver import MISInstance, MISSolver
+from conftest import simple_graph, dimacs_16nodes
+
+@pytest.mark.flaky(max_runs=5)
+@pytest.mark.parametrize("graph, default_embedder_fails", [(simple_graph(), False), (dimacs_16nodes(), True)])
+def test_easy_embedding(graph: nx.Graph, default_embedder_fails: bool) -> None:
+
+    instance = MISInstance(graph)
+    config = SolverConfig(preprocessor=None, backend = BackendConfig())
+    solver = MISSolver(instance, config)
+    assert isinstance(solver._solver._embedder, DefaultEmbedder)
+
+    register = solver.embedding()
+    assert len(register.qubits) == len(graph)
+    if default_embedder_fails:
+        with pytest.raises(Exception):
+            solver._solver.backend.device().validate_register(register)
+    else:
+        assert solver._solver.backend.device().validate_register(register) is None
+
+    opt_config = SolverConfig(preprocessor=None, backend = BackendConfig(), embedder=OptimizedEmbedder())
+    opt_solver = MISSolver(instance, opt_config)
+    assert isinstance(opt_solver._solver._embedder, OptimizedEmbedder)
+    register = opt_solver.embedding()
+    assert len(register.qubits) == len(graph)
+    assert opt_solver._solver.backend.device().validate_register(register) is None
+
