0641053cdb2a73618b9061cd81eb10ad8a3b8a1d
Charles Moussa <c.moussa@pasqal.com>
2025-10-23 09:20:11 +0200

    test class inside

--- tests/test_backends.py
+++ tests/test_backends.py
@@ -86,47 +86,70 @@ def test_local_execute(backend_kind: tuple[type[BaseBackend], BackendType], num_
         assert len(k) == 2  # Two qubits
 
 
-class TestMockServer(BaseMockServer):
-    def __init__(self) -> None:
-        super().__init__()
-        self.mocker.get("http://example.com/results/my-results", json=self.my_results)
-        self._iterations = 0
-        self._start = str(datetime.datetime.now())
-        self._runs = None
+@pytest.mark.parametrize("backend_kind", remote_backends)
+@pytest.mark.parametrize("num_shots", [1, 10, 100])
+def test_remote_execute(
+    backend_kind: tuple[type[BaseBackend], BackendType], num_shots: int
+) -> None:
+    """Test that we can run remotely a simple quantum program works."""
 
-    def endpoint_get_devices_specs(self, request: Any, context: Any, matches: list[str]) -> Any:
-        """Return a basic device called `MY_DEVICE`."""
-        return {
-            "data": {"MY_DEVICE": pulser.DigitalAnalogDevice.to_abstract_repr()},
-        }
+    # for some reason, pytest requires class inside
+    class MyMockServer(BaseMockServer):
+        def __init__(self) -> None:
+            super().__init__()
+            self.mocker.get("http://example.com/results/my-results", json=self.my_results)
+            self._iterations = 0
+            self._start = str(datetime.datetime.now())
+            self._runs = None
 
-    def endpoint_get_devices_public_specs(
-        self, request: Any, context: Any, matches: list[str]
-    ) -> Any:
-        """Return a basic device called `MY_DEVICE`."""
-        return {
-            "data": [
-                {
-                    "device_type": "MY_DEVICE",
-                    "specs": pulser.DigitalAnalogDevice.to_abstract_repr(),
-                },
-            ],
-        }
+        def endpoint_get_devices_specs(self, request: Any, context: Any, matches: list[str]) -> Any:
+            """Return a basic device called `MY_DEVICE`."""
+            return {
+                "data": {"MY_DEVICE": pulser.DigitalAnalogDevice.to_abstract_repr()},
+            }
 
-    def endpoint_post_batch(self, request: Any, context: Any, matches: list[str]) -> Any:
-        batch: dict[str, Any] = json.loads(request.text)
-        data = self.add_batch(batch)
-        return {
-            "data": data,
-        }
+        def endpoint_get_devices_public_specs(
+            self, request: Any, context: Any, matches: list[str]
+        ) -> Any:
+            """Return a basic device called `MY_DEVICE`."""
+            return {
+                "data": [
+                    {
+                        "device_type": "MY_DEVICE",
+                        "specs": pulser.DigitalAnalogDevice.to_abstract_repr(),
+                    },
+                ],
+            }
 
-    def endpoint_get_batch(self, request: Any, context: Any, matches: list[str]) -> Any:
-        """Mock for GET /api/v1/batches/{batch_id}."""
-        assert matches[0] == "my-mock-batch-id"
+        def endpoint_post_batch(self, request: Any, context: Any, matches: list[str]) -> Any:
+            batch: dict[str, Any] = json.loads(request.text)
+            data = self.add_batch(batch)
+            return {
+                "data": data,
+            }
 
-        if self._iterations < 5:
-            self._iterations += 1
-            # Initially, respond that the batch is pending.
+        def endpoint_get_batch(self, request: Any, context: Any, matches: list[str]) -> Any:
+            """Mock for GET /api/v1/batches/{batch_id}."""
+            assert matches[0] == "my-mock-batch-id"
+
+            if self._iterations < 5:
+                self._iterations += 1
+                # Initially, respond that the batch is pending.
+                return {
+                    "data": {
+                        "open": False,
+                        "complete": False,
+                        "created_at": self._start,
+                        "updated_at": str(datetime.datetime.now()),
+                        "project_id": "my-project-id",
+                        "id": "my-mock-batch-id",
+                        "user_id": "my-user-id",
+                        "status": "PENDING",
+                        "ordered_jobs": [],
+                        "device_type": "DigitalAnalogDevice",
+                    }
+                }
+
             return {
                 "data": {
                     "open": False,
@@ -136,89 +159,67 @@ class TestMockServer(BaseMockServer):
                     "project_id": "my-project-id",
                     "id": "my-mock-batch-id",
                     "user_id": "my-user-id",
-                    "status": "PENDING",
+                    "status": "DONE",
                     "ordered_jobs": [],
                     "device_type": "DigitalAnalogDevice",
                 }
             }
 
-        return {
-            "data": {
+        def endpoint_get_jobs(self, request: Any, context: Any, matches: list[str]) -> Any:
+            return {
+                "code": 200,
+                "data": [
+                    {
+                        "id": "my-job-id",
+                        "parent-id": "my-parent-id",
+                        "status": "DONE",
+                        "runs": self._runs,
+                        "batch_id": "my-match-id",
+                        "project_id": "my-project-id",
+                        "created_at": self._start,
+                        "updated_at": str(datetime.datetime.now()),
+                        "creation_order": 0,
+                    }
+                ],
+                "message": "OK.",
+                "status": "success",
+            }
+
+        def add_batch(self, batch: Any) -> Any:
+            assert batch["project_id"] == "my-project-id"
+            self._runs = batch["jobs"][0]["runs"]
+            self._register = json.loads(batch["sequence_builder"])["register"]
+            return {
                 "open": False,
                 "complete": False,
                 "created_at": self._start,
                 "updated_at": str(datetime.datetime.now()),
-                "project_id": "my-project-id",
+                "project_id": batch["project_id"],
                 "id": "my-mock-batch-id",
                 "user_id": "my-user-id",
-                "status": "DONE",
+                "status": "PENDING",
                 "ordered_jobs": [],
                 "device_type": "DigitalAnalogDevice",
             }
-        }
 
-    def endpoint_get_jobs(self, request: Any, context: Any, matches: list[str]) -> Any:
-        return {
-            "code": 200,
-            "data": [
-                {
-                    "id": "my-job-id",
-                    "parent-id": "my-parent-id",
-                    "status": "DONE",
-                    "runs": self._runs,
-                    "batch_id": "my-match-id",
-                    "project_id": "my-project-id",
-                    "created_at": self._start,
-                    "updated_at": str(datetime.datetime.now()),
-                    "creation_order": 0,
-                }
-            ],
-            "message": "OK.",
-            "status": "success",
-        }
+        def my_results(self, request: Any, context: Any) -> Any:
+            counter: Counter[str] = Counter()
+            assert isinstance(self._runs, int)
+            for _ in range(self._runs):
+                # Generate key
+                bitstr: str = ""
+                for _ in range(len(self._register)):
+                    bitstr += str(random.randrange(0, 2))
+                counter[bitstr] += 1
+            return {"counter": counter}
 
-    def add_batch(self, batch: Any) -> Any:
-        assert batch["project_id"] == "my-project-id"
-        self._runs = batch["jobs"][0]["runs"]
-        self._register = json.loads(batch["sequence_builder"])["register"]
-        return {
-            "open": False,
-            "complete": False,
-            "created_at": self._start,
-            "updated_at": str(datetime.datetime.now()),
-            "project_id": batch["project_id"],
-            "id": "my-mock-batch-id",
-            "user_id": "my-user-id",
-            "status": "PENDING",
-            "ordered_jobs": [],
-            "device_type": "DigitalAnalogDevice",
-        }
+        def endpoint_get_job_results(self, request: Any, context: Any, matches: list[str]) -> Any:
+            return {
+                "data": {
+                    "results_link": "http://example.com/results/my-results",
+                },
+            }
 
-    def my_results(self, request: Any, context: Any) -> Any:
-        counter: Counter[str] = Counter()
-        assert isinstance(self._runs, int)
-        for _ in range(self._runs):
-            # Generate key
-            bitstr: str = ""
-            for _ in range(len(self._register)):
-                bitstr += str(random.randrange(0, 2))
-            counter[bitstr] += 1
-        return {"counter": counter}
-
-    def endpoint_get_job_results(self, request: Any, context: Any, matches: list[str]) -> Any:
-        return {
-            "data": {
-                "results_link": "http://example.com/results/my-results",
-            },
-        }
-
-
-@pytest.mark.parametrize("backend_kind", remote_backends)
-@pytest.mark.parametrize("num_shots", [1, 10, 100])
-def test_remote_execute(
-    backend_kind: tuple[type[BaseBackend], BackendType], num_shots: int
-) -> None:
-    """Test that we can run remotely a simple quantum program works."""
     backend_config = BackendConfig(
         backend=backend_kind[1],
         username="my_username",
@@ -226,7 +227,7 @@ def test_remote_execute(
         project_id="my-project-id",
         device=NamedDevice("MY_DEVICE"),
     )
-    with TestMockServer():
+    with MyMockServer():
         backend = get_backend(backend_config)
         device = backend.device()
         assert device.name == "DigitalAnalogDevice"
