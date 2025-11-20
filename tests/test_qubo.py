ee1db9b27dac23e621247f0e1ef248dadc2b94ba
Charles Moussa <c.moussa@pasqal.com>
2025-11-20 12:18:41 +0100

    test qubo

---
+++ tests/test_qubo.py
@@ -0,0 +1,20 @@
+import pytest
+from conftest import simple_graph
+
+import sys
+import networkx as nx
+from mis.solver.solver import MISInstance
+
+def test_qubo_construction() -> None:
+
+    graph: nx.Graph = simple_graph()
+    if sys.version_info[1] < 13:
+        instance = MISInstance(graph)
+
+        qubo = instance.to_qubo()
+        assert qubo.size == graph.number_of_nodes()
+        assert qubo.coefficients.max().item() == 2.5
+        
+        with pytest.raises(ValueError):
+            instance.to_qubo(penalty=0.1)
+    
